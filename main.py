5692b3c1128bc2821a50d9b5d2a0786a6712a175
iBrahimHammadi <ibrahimsh2091@gmail.com>
2023-09-23 21:13:04 +0100

    Added the Game Setup

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 import pygame
-print("hello world")
+pygame.init()
+
+#Creating the window Screen
+HEIGHT, WIDTH = 600, 800
+Screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("My Platformer Game")
+
+#Creating the main function
+def main(window):
+
+    #Creating the GameLoop
+    run = True
+    while run:
+
+    #Creating the event handler
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+            
+    pygame.quit()
+    quit()
+
+if __name__ == "__main__":
+    main(Screen)
